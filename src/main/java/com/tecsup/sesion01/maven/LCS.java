515f964bde47e75b51872a961a0186cee3f899f7
Leonardo <leonardo.curaca@tecsup.edu.pe>
2019-08-27 17:30:20 -0500

    new class

---
+++ src/main/java/com/tecsup/sesion01/maven/LCS.java
@@ -0,0 +1,13 @@
+package com.tecsup.sesion01.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
