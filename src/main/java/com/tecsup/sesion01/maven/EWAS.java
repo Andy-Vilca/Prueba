8d41a4dac7baf1b0cabde6c3c0e1ba95966e6b96
EdwinArevalo <edwin.arevalo@tecsup.edu.pe>
2019-08-27 17:30:33 -0500

    commmmmit

---
+++ src/main/java/com/tecsup/sesion01/maven/EWAS.java
@@ -0,0 +1,13 @@
+package com.tecsup.sesion01.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class EWAS
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
